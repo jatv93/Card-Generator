6015c7ba15ea367c575d3a96743960c631112e4f
jatv93 <toledojennifer20@gmail.com>
2020-05-15 23:26:32 +0000

    Primer commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,35 @@
 /* eslint-disable */
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
+let RandomNumber = function() {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+};
+
+let RandomSymbol = function() {
+  let symbol = ["heart", "spade", "diamond", "club"];
+  let indexSymbol = Math.floor(Math.random() * symbol.length);
+  return symbol[indexSymbol];
+};
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(RandomSymbol());
+  document.querySelector(".card").innerHTML = RandomNumber();
 };
